10def762d0b23c80381698f3930535ba32b20425
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-06-05 12:29:13 +0200

    Add type to Post component

--- react-project/src/components/Post.tsx
+++ react-project/src/components/Post.tsx
@@ -1,7 +1,8 @@
 const names: string[] = ["Zuzana", "Maximilian"];
 
 function Post() {
-  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+  const chosenName: string = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <div>
       <p>{chosenName}</p>
