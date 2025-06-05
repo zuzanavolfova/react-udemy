2cf883a3ba9612605c1bfe566d88f9acf3f6140f
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-06-05 10:07:55 +0200

    Initial commit

---
+++ react-project/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <span>Hello</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
