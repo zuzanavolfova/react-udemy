985207cb8b3358928654555576c2642236bb1b4d
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-06-06 11:44:35 +0200

    Updating state

--- react-project/src/components/PostList.tsx
+++ react-project/src/components/PostList.tsx
@@ -1,7 +1,5 @@
 import NewPost from "./NewPost.tsx";
-import Post from "./Post.tsx";
 import Modal from "./Modal.tsx";
-import styles from "./PostList.module.css";
 import { useState } from "react";
 
 interface PostListProps {
@@ -9,31 +7,25 @@ interface PostListProps {
   onStopPosting: () => void;
 }
 
+interface PostData {
+  body: string;
+  author: string;
+}
+
 function PostList({ isPosting, onStopPosting }: PostListProps) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredName, setEnteredName] = useState("");
+  const [posts, setPosts] = useState<PostData[]>([]);
 
-  function changeBodyHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    setEnteredBody(event.target.value);
-  }
-
-  function changeNameHandler(event: React.ChangeEvent<HTMLInputElement>) {
-    setEnteredName(event.target.value);
+  function addPostsHandler(postData: PostData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={changeBodyHandler}
-            onNameChange={changeNameHandler}
-          />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostsHandler} />
         </Modal>
       )}
-      <ul className={styles.posts}>
-        <Post author={enteredName} body={enteredBody} />
-      </ul>
     </>
   );
 }
