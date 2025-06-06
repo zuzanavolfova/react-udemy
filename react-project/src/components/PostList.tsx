1f0f3cbdac6479d8f9062778bae75297bc887641
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-06-06 09:56:36 +0200

    Add state to PostList, Use modal

--- react-project/src/components/PostList.tsx
+++ react-project/src/components/PostList.tsx
@@ -1,12 +1,33 @@
+import NewPost from "./NewPost.tsx";
 import Post from "./Post.tsx";
+import Modal from "./Modal.tsx";
 import styles from "./PostList.module.css";
+import { useState } from "react";
 
 function PostList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+
+  function changeBodyHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setEnteredBody(event.target.value);
+  }
+
+  function changeNameHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    setEnteredName(event.target.value);
+  }
+
   return (
-    <ul className={styles.posts}>
-      <Post author=" Zuzana" body="React is fine" />
-      <Post author="Max" body=" He is lector" />
-    </ul>
+    <>
+      <Modal>
+        <NewPost
+          onBodyChange={changeBodyHandler}
+          onNameChange={changeNameHandler}
+        />
+      </Modal>
+      <ul className={styles.posts}>
+        <Post author={enteredName} body={enteredBody} />
+      </ul>
+    </>
   );
 }
 
