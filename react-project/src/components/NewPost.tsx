70260b15f874ed76e2d10665b33683e57014240f
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-06-06 09:57:17 +0200

    Add state to NewPost component

--- react-project/src/components/NewPost.tsx
+++ react-project/src/components/NewPost.tsx
@@ -1,15 +1,21 @@
 import styles from "./NewPost.module.css";
+import type React from "react";
 
-function NewPost() {
+interface NewPostProps {
+  onBodyChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  onNameChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function NewPost(props: NewPostProps) {
   return (
     <form className={styles.form}>
       <p>
         <label htmlFor="body"> Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onNameChange} />
       </p>
     </form>
   );
