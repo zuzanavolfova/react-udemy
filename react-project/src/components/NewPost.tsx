7a113891bdfa3d351c50d221692696accde9e37a
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-06-06 11:44:00 +0200

    Add submit handler

--- react-project/src/components/NewPost.tsx
+++ react-project/src/components/NewPost.tsx
@@ -1,17 +1,43 @@
 import styles from "./NewPost.module.css";
+import { useState } from "react";
 import type React from "react";
 
 interface NewPostProps {
-  onBodyChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
-  onNameChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onCancel: () => void;
+  onAddPost: (postData: PostData) => void;
+}
+interface PostData {
+  body: string;
+  author: string;
 }
 
-function NewPost(props: NewPostProps) {
+function NewPost({ onCancel, onAddPost }: NewPostProps) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+
+  function changeBodyHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setEnteredBody(event.target.value);
+  }
+
+  function changeNameHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    setEnteredName(event.target.value);
+  }
+
+  function submitHandler(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const postData: PostData = {
+      body: enteredBody,
+      author: enteredName,
+    };
+    onAddPost(postData);
+    onCancel();
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body"> Text</label>
-        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
